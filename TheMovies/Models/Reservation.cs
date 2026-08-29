197b2f028c4dd2b70a37fcd5e836b8cebd62e66a
Anika <anika.fuglsang@gmail.com>
2026-08-29 12:52:17 +0200

    Add reservation model

---
+++ TheMovies/Models/Reservation.cs
@@ -0,0 +1,17 @@
+namespace TheMovies.Models
+{
+    public class Reservation
+    {
+        public Screening Forestilling
+        { get; set; }
+
+        public int AntalBilletter
+        { get; set; }
+
+        public string Email
+        { get; set; }
+
+        public string Telefonnummer
+        { get; set; }
+    }
+}
